a5831e8f2d56a12af8c73960f6ec0d48dc9ef51b
xindihuo <xindihuo@163.com>
2018-11-12 15:13:16 +0800

    test_jenkins

---
+++ test_jenkins.py
@@ -0,0 +1,14 @@
+class TestJenkins:
+    def test_jenkins1(self):
+        print("jenkins1")
+        assert 1
+
+    def test_jenkins2(self):
+        print("jenkins2")
+        assert 1
+
+    def test_jenkins3(self):
+        print("jenkins3")
+        assert 0
+if __name__ == '__main__':
+    print()
